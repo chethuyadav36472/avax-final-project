a00511236773c52e4aaaeed3955c64bfff741fcd
chethuyadav36472 <165311759+chethuyadav36472@users.noreply.github.com>
2024-04-26 11:48:41 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "chethan yadav"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "yadavv"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "CY"
 )
 
 var ID ids.ID
